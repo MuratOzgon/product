26143c66899ff3e945be614793ffb7680fef4456
muratozgon <muratozgon@gmail.com>
2020-11-06 02:33:35 +0300

    Mysql connected.

--- src/main/java/org/springframework/boot/product/Product.java
+++ src/main/java/org/springframework/boot/product/Product.java
@@ -2,13 +2,11 @@ package org.springframework.boot.product;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
+@Table(name = "products")
 public class Product {
 
     @Id
